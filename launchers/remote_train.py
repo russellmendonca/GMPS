3a2f3e99b08f8b333d7327cc718a9a2ca463e434
Zhiwei-Z <zhiweizhang2012@gmail.com>
2020-01-13 18:06:41 -0800

    changing path

--- launchers/remote_train.py
+++ launchers/remote_train.py
@@ -1,9 +1,10 @@
-from sandbox.rocky.tf.algos.maml_il import MAMLIL
 import sys
 sys.path.append("../R_multiworld")
 from rllab.misc.comet_logger import CometLogger
 comet_logger = CometLogger(api_key="KWwx7zh6I2uw6oQMkpEo3smu0",
                             project_name="ml4l3", workspace="glenb")
+from sandbox.rocky.tf.algos.maml_il import MAMLIL
+
 from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.baselines.gaussian_mlp_baseline import GaussianMLPBaseline
 from rllab.baselines.maml_gaussian_mlp_baseline import MAMLGaussianMLPBaseline
