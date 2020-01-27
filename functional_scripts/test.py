370364e81550b4bf485dd46e68450cd4f6bd55dd
Zhiwei-Z <zhiweizhang2012@gmail.com>
2020-01-27 01:16:17 -0800

    test

--- functional_scripts/test.py
+++ functional_scripts/test.py
@@ -10,20 +10,18 @@ EXPERT_DATA_LOC = test_dir + '/seq_expert_traj/'
 exploc2 = path_to_gmps + '/saved_expert_trajs/ant-quat-v2-10tasks-itr400/'
 comp1 = path_to_gmps + "/russel.txt"
 comp2 = path_to_gmps + "/zzw.txt"
-with tf.Session().as_default as sess:
-    expert_traces = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(4)}
-    # for taskidx in range(4):
-    #     print(expert_traces[taskidx])
-    file1 = open(comp1, 'w')
-    file2 = open(comp2, 'w')
-    file1.write(str(expert_traces[0]))
-    file2.write(str(expert_traces[3]))
-    file1.close()
-    file2.close()
-
-sess.close()
-
-# expert_traces2 = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(3)}
+# with tf.Session().as_default as sess:
+#     expert_traces = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(4)}
+#     # for taskidx in range(4):
+#     #     print(expert_traces[taskidx])
+#     file1 = open(comp1, 'w')
+#     file2 = open(comp2, 'w')
+#     file1.write(str(expert_traces[0]))
+#     file2.write(str(expert_traces[3]))
+#     file1.close()
+#     file2.close()
 #
-# for taskidx in range(3):
-#     print(expert_traces2[taskidx])
+# sess.close()
+
+expert_traces = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(4)}
+print(expert_traces[0])
