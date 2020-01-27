68828998e38aa788d78b96693de1ee5036c689e9
Zhiwei-Z <zhiweizhang2012@gmail.com>
2020-01-27 00:30:51 -0800

    change test

--- functional_scripts/test.py
+++ functional_scripts/test.py
@@ -9,8 +9,10 @@ EXPERT_DATA_LOC = test_dir + '/seq_expert_traj/'
 exploc2 = path_to_gmps + '/saved_expert_trajs/ant-quat-v2-10tasks-itr400/'
 
 expert_traces = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(3)}
-for taskidx in range(4):
-    print(expert_traces[taskidx])
+# for taskidx in range(4):
+#     print(expert_traces[taskidx])
+print(expert_traces[0])
+print(expert_traces[1])
 
 # expert_traces2 = {taskidx : joblib.load(EXPERT_DATA_LOC+str(taskidx)+".pkl") for taskidx in range(3)}
 #
