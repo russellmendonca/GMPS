fc91511ee816325c630329ebc03dfce909b727d7
Zhiwei-Z <zhiweizhang2012@gmail.com>
2020-01-28 23:24:43 -0800

    what if just save paths

--- sandbox/rocky/tf/algos/batch_polopt.py
+++ sandbox/rocky/tf/algos/batch_polopt.py
@@ -218,6 +218,7 @@ class BatchPolopt(RLAlgorithm):
                         params["paths"] = samples_data["paths"]
 
                     logger.save_itr_params(itr, params, file_name=str(self.reset_arg)+'.pkl')
+                    print(params['paths'])
                     logger.log("Saved")
                     logger.log("Optimizing policy...")
                     self.optimize_policy(itr, samples_data)
